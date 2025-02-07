06cca595642618e79a295f322110a7f4eeecfd94
mohamedislemdjaballah <med.islam07@hotmail.com>
2025-02-08 00:32:42 +0100

    handle the request in a controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// our first route is a get request to the /reviews endpoint
+
+// Route::get('/reviews', function(){
+//     return response()->json([
+//         'message' => 'This is a get request'
+//     ]);
+// });
+
+// we can develop to using a controller to handle the request
+Route::get('/reviews', [ReviewController::class,'index']);
