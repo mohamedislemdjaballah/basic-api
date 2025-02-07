8039c8d7ffa54c999e613de2c26742fabb0211f7
mohamedislemdjaballah <med.islam07@hotmail.com>
2025-02-08 00:43:53 +0100

    back to the original

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,12 @@ use App\Http\Controllers\ReviewController;
 
 // our first route is a get request to the /reviews endpoint
 
-// Route::get('/reviews', function(){
-//     return response()->json([
-//         'message' => 'This is a get request'
-//     ]);
-// });
+Route::get('/reviews', function(){
+    return response()->json([
+        'message' => 'This is a get request'
+    ]);
+});
 
 // we can develop to using a controller to handle the request
-Route::get('/reviews', [ReviewController::class,'index']);
+
+// Route::get('/reviews', [ReviewController::class,'index']);
